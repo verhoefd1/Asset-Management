3b991918289a6f825046e6d5097f7194a8c3a61e
Dylan <verhoefd1@gmail.com>
2020-03-12 15:02:31 -0500

    Added in files, not to plan and build scaffolding.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+
+//import and initialize variales and modules.
 var express		= require("express"),
 app				= express(),
+//REST API - express is the framework to use REST API with Node
 mongoose		= require("mongoose"),
+//Mongoose interacts with MongoDB
 port 			= 8080;
+//sets listening port for site. 
+
+
+
 
 app.use(express.static(__dirname + "/public"));
-
+//serves all files in public directly to / 
 mongoose.connect("mongodb://localhost/assetManagement");
-
+//Connects with database
 app.set("views", __dirname + "/views/");
 app.set("view engine", "ejs");
+//adds the middleware to all the routes without explicitly adding it. 
 
 app.get("/", function(req, res){
 	res.send("Hello World");
@@ -16,5 +25,4 @@ app.get("/", function(req, res){
 
 app.listen(port, function(){
 	console.log("Asset Management Application listening on: " + port);
-
 });
