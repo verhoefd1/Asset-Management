4135ce7bcd60787327992ad70d3b1b0b0cc2594e
Dylan <verhoefd1@gmail.com>
2020-04-14 17:30:17 -0500

    Hello world complete

--- app.js
+++ app.js
@@ -8,19 +8,16 @@ mongoose		= require("mongoose"),
 port 			= 8080;
 //sets listening port for site. 
 
-
-
-
 app.use(express.static(__dirname + "/public"));
 //serves all files in public directly to / 
 mongoose.connect("mongodb://localhost/assetManagement");
 //Connects with database
-app.set("views", __dirname + "/views/");
+app.set("views",__dirname + "/views/");
 app.set("view engine", "ejs");
 //adds the middleware to all the routes without explicitly adding it. 
 
 app.get("/", function(req, res){
-	res.send("Hello World");
+	res.render("index");
 });
 
 app.listen(port, function(){
